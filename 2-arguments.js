b4def369e1e02208788f4fd81983acf7826f0f63
Peninah <165781054+Peninah-Wambui@users.noreply.github.com>
2025-11-13 04:25:53 -0800

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args= process.argv.slice(2);
+if (args.length ==0)[
+    console.log("No arguement")
+]
+else if (args.length ==1)[
+    console.log("arguement found")
+]
+else [
+    console.log("arguement found")
+]
